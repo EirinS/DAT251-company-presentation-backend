d8e301b9d843c93998e08cbc70e2a4b30360bf26
Eirin Sognnes <eirin.sognnes@outlook.com>
2020-03-13 20:46:23 +0100

    Add application.properties for tests

--- src/test/java/com/presentation/MainControllerMockTest.java
+++ src/test/java/com/presentation/MainControllerMockTest.java
@@ -1,15 +1,16 @@
 package com.presentation;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.transaction.Transactional;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(classes=AccessingDataMysqlApplication.class)
+@SpringBootTest()
 @AutoConfigureMockMvc
 public class MainControllerMockTest {
 
