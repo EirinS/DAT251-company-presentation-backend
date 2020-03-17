fe53efcb911d1bc7f0b06f31f04e1cae6b7fe61b
HeLiHex <henrik.lh@hotmail.com>
2020-03-17 18:38:43 +0100

    CompanyRepository implements company and not Presentation

--- src/main/java/com/presentation/repositories/CompanyRepository.java
+++ src/main/java/com/presentation/repositories/CompanyRepository.java
@@ -1,8 +1,9 @@
 package com.presentation.repositories;
 
-import com.presentation.entities.Presentation;
+import com.presentation.entities.Company;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CompanyRepository extends CrudRepository<Presentation, Integer> {
+public interface CompanyRepository extends CrudRepository<Company, Integer> {
+
 
 }
