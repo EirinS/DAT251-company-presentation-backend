3fc387e7b47c671e419d09bf3f32de4858074b03
Eirin Sognnes <eirin.sognnes@outlook.com>
2020-03-19 13:21:21 +0100

    Remove unused imports

--- src/test/java/com/presentation/UserTest.java
+++ src/test/java/com/presentation/UserTest.java
@@ -1,26 +1,19 @@
 package com.presentation;
 
-import com.presentation.AccessingDataMysqlApplication;
 import com.presentation.entities.User;
 import com.presentation.repositories.UserRepository;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
 import org.springframework.test.context.junit4.SpringRunner;
 import java.util.Iterator;
-import java.util.List;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes= AccessingDataMysqlApplication.class)
+@DataJpaTest
 public class UserTest {
 
     @Autowired
