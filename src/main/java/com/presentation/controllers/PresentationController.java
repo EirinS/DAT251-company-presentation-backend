858827815294b7fe6ab33e925f1b8f1f3b0e81cc
EirinS <eirin.sognnes@outlook.com>
2020-03-31 11:36:39 +0200

    Change exception thrown from NotFound to IllegalArgument
    Fix imports

--- src/main/java/com/presentation/controllers/PresentationController.java
+++ src/main/java/com/presentation/controllers/PresentationController.java
@@ -2,10 +2,7 @@ package com.presentation.controllers;
 
 import com.presentation.entities.Company;
 import com.presentation.entities.Presentation;
-import com.presentation.entities.User;
-import com.presentation.entities.UserAttendingPresentation;
 import com.presentation.repositories.PresentationRepository;
-import org.omg.CosNaming.NamingContextPackage.NotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.ReflectionUtils;
@@ -14,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import java.lang.reflect.Field;
 import java.sql.Date;
 import java.util.Map;
-import java.util.Optional;
 
 @Controller
 public class PresentationController {
@@ -52,8 +48,8 @@ public class PresentationController {
 
     @PatchMapping(path = "/editPresentation/{id}")
     public @ResponseBody
-    Presentation editPresentation(@PathVariable("id") Integer id, @RequestBody Map<String, Object> fields) throws NotFound {
-        Presentation presentation = presentationRepository.findById(id).orElseThrow(NotFound::new);
+    Presentation editPresentation(@PathVariable("id") Integer id, @RequestBody Map<String, Object> fields) {
+        Presentation presentation = presentationRepository.findById(id).orElseThrow(IllegalArgumentException::new);
         fields.forEach((k, v) -> {
             Field field = ReflectionUtils.findField(Presentation.class, k);
             field.setAccessible(true);
